bf58b525990fefb5a55de7283ef5a2a723740e48
rinae-del <rvumby@gmail.com>
2024-09-06 19:33:52 +0200

    ...

--- appwrite-file-upload/index.js
+++ appwrite-file-upload/index.js
@@ -1,5 +1,4 @@
-import { Client, Databases, Query } from 'node-appwrite';
-
-export default async function ({ req, res }) {
+const { Client, Databases, Query } = require('node-appwrite');
+module.exports = async function ({ req, res }) {
   return res.json({ ok: true, message: 'It works!' });
 };
