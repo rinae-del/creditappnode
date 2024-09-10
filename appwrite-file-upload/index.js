e85b790ba7e6aeda2afe6b150a0626e720face40
rinae-del <rvumby@gmail.com>
2024-09-10 19:12:41 +0200

    ..

--- appwrite-file-upload/index.js
+++ appwrite-file-upload/index.js
@@ -1,55 +1,55 @@
-const https = require('https');
-const fs = require('fs');;
-const { Client, Databases, Storage, ID, InputFile } = require('node-appwrite');
+const sdk = require('node-appwrite');
 
-const client = new Client()
-            .setEndpoint('https://cloud.appwrite.io/v1')
-            .setProject('66b0ff530019305177ae')
-            .setKey('8fb30e76842b1ccd2131723bf09212db2ad90c019f9191bcfc001682739ecc9c7987543270cd706577be049314085d2175528006ea118dd4d5fa4d7249156d0d3f305c9a7da2c716cecb3302cb1abdcca8ec33cee1b730e3272708485abf8ca82ff2add77d34febb6d65da45945a7881e4c4038f8d924eaa4096fd11d70a53c9');
-
-
-const storage = new Storage(client);
-
-module.exports = async function ({req, res}) {
-    const imageUrl = 'https://doc.nuxeo.com/assets/nxdoc/university/university-rest-api-importer.png';
-
-    const tempFilePath = '/tmp/university-rest-api-importer.png';
-
+module.exports = async function (req, res) {
     try {
-        await new Promise((resolve, reject) => {
-            https.get(imageUrl, (response) => {
-                const file = fs.createWriteStream(tempFilePath);
-                response.pipe(file);
+        const client = new sdk.Client();
+        const storage = new sdk.Storage(client);
 
-                file.on('finish', () => {
-                    file.close(resolve);
-                });
+        client
+            .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+            .setProject('66b0ff530019305177ae')         // Your Appwrite Project ID
+            .setKey('8fb30e76842b1ccd2131723bf09212db2ad90c019f9191bcfc001682739ecc9c7987543270cd706577be049314085d2175528006ea118dd4d5fa4d7249156d0d3f305c9a7da2c716cecb3302cb1abdcca8ec33cee1b730e3272708485abf8ca82ff2add77d34febb6d65da45945a7881e4c4038f8d924eaa4096fd11d70a53c9');                    // Your API Key
 
-                file.on('error', (err) => {
-                    fs.unlink(tempFilePath, () => {}); 
-                    reject(err);
-                });
-            }).on('error', (err) => {
-                reject(err);
+        const fileId = sdk.ID.unique();  // Generate a unique ID for the file
+        const bucketId = '66d9dd600031db125daf'; // Your Appwrite bucket ID
+        const uri = req.variables['profileFromStorage']; // Get the image URI from function input
+        const match = /\.(\w+)$/.exec(uri);
+        const fileType = match ? `image/${match[1]}` : `image/jpeg`;
+        const fileName = `profile_picture.${match[1]}`;
+
+        // Fetch the image from the provided URI
+        const response = await fetch(uri);
+        const blob = await response.blob();
+        const buffer = await blob.arrayBuffer();
+
+        // Upload the file to Appwrite Storage
+        const result = await storage.createFile(
+            bucketId,
+            fileId,
+            buffer, 
+            fileName, // File name
+            ['role:all'] // Define permissions
+        );
+
+        if (result) {
+            console.log('Image uploaded successfully:', result);
+            return res.json({
+                success: true,
+                message: 'Image uploaded successfully',
+                result: result
             });
-        });
-
-        const bucketId = '66d9dd600031db125daf'; 
-        const fileId = 'university-rest-api-importer.png'; 
-
-        const fileStream = fs.createReadStream(tempFilePath);
-        const result = await storage.createFile(bucketId, fileId, fileStream, 'image/png');
-        
-        fs.unlinkSync(tempFilePath);
-
-        return res.json({
-            success: true,
-            result
-        });
+        } else {
+            return res.json({
+                success: false,
+                message: 'Failed to upload image',
+            });
+        }
     } catch (error) {
+        console.error('Error uploading image:', error);
         return res.json({
             success: false,
-            error: error.message
+            message: 'Error uploading image',
+            error: error.toString(),
         });
     }
 };
