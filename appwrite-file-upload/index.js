e5aa3f7c9e9b3cbb087e666e439bfdd6e03322f3
rinae-del <rvumby@gmail.com>
2024-09-10 19:32:45 +0200

    ...

--- appwrite-file-upload/index.js
+++ appwrite-file-upload/index.js
@@ -12,7 +12,13 @@ module.exports = async function ({req, res}) {
 
         const fileId = sdk.ID.unique();  // Generate a unique ID for the file
         const bucketId = '66d9dd600031db125daf'; // Your Appwrite bucket ID
-        const uri = req.variables['profileFromStorage']; // Get the image URI from function input
+        // const uri = req.variables['profileFromStorage']; // Get the image URI from function input
+        return res.json({
+            success: false,
+            message: 'Image uploaded successfully',
+            result: 'result',
+            body: req.body,
+        });
         const match = /\.(\w+)$/.exec(uri);
         const fileType = match ? `image/${match[1]}` : `image/jpeg`;
         const fileName = `profile_picture.${match[1]}`;
